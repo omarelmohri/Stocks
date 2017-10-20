6af076fcf02c898104f23711deab43a3b30f1da0
Omar E <omar.elmohri@gmail.com>
2017-10-19 22:46:41 -0400

    Add files via upload

---
+++ stocks/portfolio.py
@@ -0,0 +1,42 @@
+from yahoo_finance import Share
+
+
+
+class Position(object):
+
+    def __init__(self,ticker,qty,price,fee = 0):
+        self.ticker = ticker
+        self.qty = int(qty)
+        self.price = float(price)
+        self.fee = float(fee)
+        self.yPointer = Share(self.ticker)
+        self.name = self.yPointer.get_name()
+        try:
+            self.info = self.yPointer.get_info()
+        except:
+            self.info = ""
+
+    def totalCost(self):
+        tc = float(self.qty) * float(self.price)
+        return tc
+
+    def currentPrice(self):
+        return float(self.yPointer.get_price())
+
+    def marketValue(self):
+        mV = (self.qty * float(self.yPointer.get_price())) + self.fee
+        return mV
+
+    def positionGain(self):
+        diff = self.marketValue() - self.totalCost()
+        return diff
+
+    def positionGainPercent(self):
+        return (1 - self.totalCost()/self.marketValue())
+
+
+
+#Get price from Yahoo
+#stk = Share(ticker)
+#price = float(stk.get_price())
+#pcent_change = str(stk.get_percent_change())
